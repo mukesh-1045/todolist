3789ff8d256ea8c131726dd878d78dc2adf8fd55
Mukesh buldak <mukeshkumar.buldak12@gmail.com>
2021-08-31 20:50:24 +0530

    final work done

---
+++ src/app/scholarship-module/scholarship-module.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViewScholarshipComponent } from './view-scholarship/view-scholarship.component';
+
+
+
+@NgModule({
+  declarations: [
+    ViewScholarshipComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ScholarshipModuleModule { }
---
+++ src/app/scholarship-module/view-scholarship/view-scholarship.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-scholarship',
+  templateUrl: './view-scholarship.component.html',
+  styleUrls: ['./view-scholarship.component.css']
+})
+export class ViewScholarshipComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
